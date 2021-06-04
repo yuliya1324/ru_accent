62fdcd79b905814849cb03c2595ff5435d194858
Boris Orekhov <nevmenandr@gmail.com>
2021-06-04 15:10:56 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["requests>=2.21.0", "russtress"]
 
 setuptools.setup(
     name="ru_accent_poet",
-    version="0.1.3",
+    version="0.1.4",
     author="Julia Korotkova",
     author_email="koylenka15@gmail.com",
     description="A package for putting stress marks in russian poetic texts",
